1336522211df4cd044ae56b675a5ad9d648d268c
Ankur Shrivastava <ankur@ankurs.com>
2026-03-22 21:14:19 +0800

    docs: document intentional uint32 overflow in round-robin selector

--- pool.go
+++ pool.go
@@ -46,6 +46,8 @@ func (p *roundRobinConnPool) Num() int {
 }
 
 func (p *roundRobinConnPool) Conn() *grpc.ClientConn {
+	// Overflow at math.MaxUint32 is intentional and correct: the modulo
+	// operation still distributes evenly across connections after wrap-around.
 	i := atomic.AddUint32(&p.idx, 1)
 	return p.conns[i%uint32(len(p.conns))]
 }
